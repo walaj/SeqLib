297997afeb432bc1d14d9a527d7a0caf7c115278
Jeremiah Wala <jwala@broadinstitute.org>
2016-01-12 15:09:25 -0500

    added coveralls support

---
+++ snow_test/snow_test.cpp
@@ -0,0 +1,17 @@
+#include <climits>
+#include <boost/test/unit_test.hpp>
+
+#include "SnowTools/MiniRules.h"
+
+BOOST_AUTO_TEST_CASE( test_test ) {
+
+  BOOST_TEST( true /* test assertion */ );
+
+  //std::string rules = "pad[100];mlregion@test_vcf.vcf%clip[5,100]";
+  //SnowTools::MiniRulesCollection mr(rules);
+
+  //BOOST_CHECK_EQUAL( mr.hasWholeGenome(), false );
+  //BOOST_CHECK_EQUAL( mr.numRules(), 1 );  
+  //BOOST_CHECK_EQUAL( mr.numRules(), 2 );  
+
+}
