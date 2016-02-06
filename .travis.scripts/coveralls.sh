970fe38f6a6ea4d6bf04cb74dcb2f74627ac2dab
Jeremiah Wala <jwala@broadinstitute.org>
2016-02-06 16:02:06 -0500

    doxy update

--- .travis.scripts/coveralls.sh
+++ .travis.scripts/coveralls.sh
@@ -12,7 +12,7 @@ echo "...running unit tests and code coverage"
   make
   rm snow_test-snow-test.gcno ## dont get cov on test prog
   ./snow_test
-  EXCL="-e snow_test/snow_test.cpp -e htslib -e bwa -e multifast-v1.4.2 -e blat -e tools -e SGA -e snow_test/snow_test_main.cpp -e R -e rtdocs -e examples -e doxy"
+  EXCL="-e snow_test/snow_test.cpp -e htslib -e bwa -e multifast-v1.4.2 -e blat -e tools -e SGA -e snow_test/snow_test_main.cpp -e R -e rtdocs -e examples -e doxy -e src/deprecated.h -e config.h -e snow_test/config.h -e src/SnowTools/gzstream.h -e snow_test/config.h"
   cpp-coveralls -r ../ -t ${COVERALLS_TOKEN} ${EXCL} --dryrun
   find ./ -type f -regex ".*/[a-z_]+.*gcov" -delete
   cpp-coveralls -r ../ -t ${COVERALLS_TOKEN} ${EXCL} -n
