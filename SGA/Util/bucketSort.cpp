5a96bb5444f2114e9c1f555223eaf2be6a5c3835
Jeremiah Wala <jwala@broadinstitute.org>
2016-07-31 13:17:48 -0400

    switching fermi kit in for SGA

--- SGA/Util/bucketSort.cpp
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------
-// Copyright 2009 Wellcome Trust Sanger Institute
-// Written by Jared Simpson (js18@sanger.ac.uk)
-// Released under the GPL
-//-----------------------------------------------
-//
-// bucketSort - perform a bucket sort over a range of values
-//
-
