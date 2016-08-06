ad3e377ca41b9885c1caca77f0e6581f95505769
Jeremiah Wala <jwala@broadinstitute.org>
2016-08-06 17:59:16 -0400

    travis2

--- .travis.scripts/travis-before-install.sh
+++ .travis.scripts/travis-before-install.sh
@@ -8,9 +8,9 @@ if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   brew update
 fi
 
-#if [ -n "${BOOST_VERSION}" ]; then
-#  mkdir -p $BOOST_ROOT
-#  wget --no-verbose --output-document=- \
-#    http://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/boost_${BOOST_VERSION//./_}.tar.bz2/download \
-#    | tar jxf - --strip-components=1 -C "${BOOST_ROOT}"
-#fi
+if [ -n "${BOOST_VERSION}" ]; then
+    mkdir -p $BOOST_ROOT
+    wget --no-verbose --output-document=- \
+	http://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/boost_${BOOST_VERSION//./_}.tar.bz2/download \
+	| tar jxf - --strip-components=1 -C "${BOOST_ROOT}"
+fi
