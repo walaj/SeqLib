6e3b0917ae954cb9b57587c0f7d166714f812e59
Jeremiah Wala <jwala@broadinstitute.org>
2016-08-10 23:28:49 -0400

    cover fix

--- .travis.scripts/coveralls.sh
+++ .travis.scripts/coveralls.sh
@@ -19,7 +19,7 @@ echo "...running unit tests and code coverage"
   rm snow_test-snow-test.gcno ## dont get cov on test prog
 
   ./snow_test
-  EXCL="-e snow_test/snow_test.cpp -e htslib -e bwa -e tools -e snow_test/snow_test_main.cpp -e R -e examples -e doxy -e src/deprecated.h -e config.h -e snow_test/config.h -e src/SnowTools/gzstream.h -e snow_test/config.h"
+  EXCL="-e snow_test/snow_test.cpp -e htslib -e bwa -e tools -e snow_test/snow_test_main.cpp -e R -e examples -e doxy -e src/deprecated.h -e fermi-lite -e config.h -e snow_test/config.h -e src/SnowTools/gzstream.h -e snow_test/config.h -e src/jsoncpp.cpp -e src/json -e multifast-v1.4.2"
   cpp-coveralls -r ../ -t ${COVERALLS_TOKEN} ${EXCL} ##--dryrun
   #find ./ -type f -regex ".*/[a-z_]+.*gcov" -delete
   #cpp-coveralls -r ../ -t ${COVERALLS_TOKEN} ${EXCL} -n
