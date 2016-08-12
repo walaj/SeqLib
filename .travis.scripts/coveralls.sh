d2a355bfe7ba127d9d61d58c830e43ab25a3ffb2
Jeremiah Wala <jwala@broadinstitute.org>
2016-08-11 20:44:39 -0400

    coveralls

--- .travis.scripts/coveralls.sh
+++ .travis.scripts/coveralls.sh
@@ -14,11 +14,13 @@ echo "...running unit tests and code coverage"
   fi
   cd snow_test
   
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${BOOST_ROOT}/stage/lib
+  echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
   ./configure --with-boost=${BOOST_ROOT}
   make
-  rm snow_test-snow-test.gcno ## dont get cov on test prog
-
   ./snow_test
+  #rm snow_test-snow-test.gcno ## dont get cov on test prog
+
   EXCL="-e snow_test/snow_test.cpp -e htslib -e bwa -e tools -e snow_test/snow_test_main.cpp -e R -e examples -e doxy -e src/deprecated.h -e fermi-lite -e config.h -e snow_test/config.h -e src/SnowTools/gzstream.h -e snow_test/config.h -e src/jsoncpp.cpp -e src/json -e multifast-v1.4.2"
   cpp-coveralls -r ../ -t ${COVERALLS_TOKEN} ${EXCL} ##--dryrun
   #find ./ -type f -regex ".*/[a-z_]+.*gcov" -delete
