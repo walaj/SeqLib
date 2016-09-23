cd3c3272ad711d11ea100d0f578b33219f4244b1
Jeremiah Wala <jwala@broadinstitute.org>
2016-09-23 13:51:06 -0400

    fix simpler build matrix

--- .travis.scripts/travis-install.sh
+++ .travis.scripts/travis-install.sh
@@ -4,10 +4,10 @@ set -e
 set -o pipefail
 
 ## only build for one compiler
-VALID=`g++ --version | grep 4.9 | wc -l`
-if [[ "$VALID" -eq "0" ]]; then
-  exit 0;
-fi
+#VALID=`g++ --version | grep 4.9 | wc -l`
+#if [[ "$VALID" -eq "0" ]]; then
+#  exit 0;
+#fi
 
 if [ -d "${BOOST_ROOT}" ]; then
   (cd "${BOOST_ROOT}"
